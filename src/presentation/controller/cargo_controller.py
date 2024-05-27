011571af157573d6705f17818b8265687722365e
MolitorLucas <lucasmolitor10@gmail.com>
2024-05-27 16:18:16 -0300

    Métodos atualizar e excluir cargo no controller

--- src/presentation/controller/cargo_controller.py
+++ src/presentation/controller/cargo_controller.py
@@ -3,6 +3,7 @@ from src.domain.models import Cargo
 from src.presentation.http_types import HttpRequest, HttpResponse
 
 class CargoController():
+
     @classmethod
     def __init__(self, use_case: CargoUseCasesInterface):
         self.__use_case = use_case
@@ -28,6 +29,23 @@ class CargoController():
     @classmethod
     def buscar_cargo_por_id(self, request: HttpRequest) -> HttpResponse: 
         response = self.__use_case.buscar_cargo_por_id(request.query_params["id"])
+        return HttpResponse (
+            status_code=200,
+            body = {"data": response}
+        )
+    
+    @classmethod
+    def atualizar(self, request: HttpRequest) -> HttpResponse: 
+        form = Cargo(request.body["id"], request.body["descricao"])
+        response = self.__use_case.atualizar(form)
+        return HttpResponse (
+            status_code=200,
+            body = {"data": response}
+        )
+    
+    @classmethod
+    def excluir(self, request: HttpRequest) -> HttpResponse: 
+        response = self.__use_case.excluir(request.query_params["id"])
         return HttpResponse (
             status_code=200,
             body = {"data": response}
