9e084a354d9121b06ed1fe1c7ba215201de1ad7d
MolitorLucas <lucasmolitor10@gmail.com>
2024-06-04 17:56:14 -0300

    Controller manter tipo evento

---
+++ src/presentation/controller/tipo_evento/manter_tipo_evento.py
@@ -0,0 +1,52 @@
+from src.domain.use_cases.tipo_evento import ManterTipoEventoInterface
+from src.domain.models import TipoEvento
+from src.presentation.http_types import HttpRequest, HttpResponse
+
+class ManterTipoEventoController():
+
+    @classmethod
+    def __init__(self, use_case: ManterTipoEventoInterface):
+        self.__use_case = use_case
+    
+    @classmethod
+    def buscar(self, request: HttpRequest) -> HttpResponse: 
+        response = self.__use_case.buscar_tipo_eventos()
+        return HttpResponse(
+            status_code=200,
+            body = {"data": response}
+        )
+
+    @classmethod
+    def cadastrar(self, request: HttpRequest) -> HttpResponse:
+        form = TipoEvento(0, request.body["descricao"])
+        response = self.__use_case.cadastrar(form)
+
+        return HttpResponse(
+            status_code=200,
+            body = { "data": response }
+        )
+
+    @classmethod
+    def buscar_por_id(self, request: HttpRequest) -> HttpResponse: 
+        response = self.__use_case.buscar_tipo_evento_por_id(request.query_params["id"])
+        return HttpResponse (
+            status_code=200,
+            body = {"data": response}
+        )
+    
+    @classmethod
+    def atualizar(self, request: HttpRequest) -> HttpResponse: 
+        form = TipoEvento(request.body["id"], request.body["descricao"])
+        response = self.__use_case.atualizar(form)
+        return HttpResponse (
+            status_code=200,
+            body = {"data": response}
+        )
+    
+    @classmethod
+    def excluir(self, request: HttpRequest) -> HttpResponse: 
+        response = self.__use_case.excluir(request.query_params["id"])
+        return HttpResponse (
+            status_code=200,
+            body = {"data": response}
+        )
