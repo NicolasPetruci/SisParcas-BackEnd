08fa1147c2e9b37da8caa5aea4635d518c6291d9
MolitorLucas <lucasmolitor10@gmail.com>
2024-06-11 14:06:01 -0300

    Correção no campo 'online' no parse json de evento

--- src/domain/models/evento.py
+++ src/domain/models/evento.py
@@ -106,6 +106,7 @@ class Evento():
             "nome": self.nome,
             "descricao": self.descricao, 
             "local": self.local,
+            "online": "Sim" if self.local else "Não",
             "data_hora": self.data_hora,
             "tipo_evento": self.tipo_evento.to_json() if self.tipo_evento else None,
             "participantes": [p.to_json() for p in self.participantes]
